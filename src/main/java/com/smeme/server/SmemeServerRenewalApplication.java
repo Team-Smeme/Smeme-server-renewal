45ca9e75157f7d4ac08cf302542030ff9ff46e89
thguss <thgus345@naver.com>
2023-01-12 21:28:09 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/smeme/server/SmemeServerRenewalApplication.java
@@ -0,0 +1,13 @@
+package com.smeme.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmemeServerRenewalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmemeServerRenewalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smeme/server/SmemeServerRenewalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smeme.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmemeServerRenewalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
