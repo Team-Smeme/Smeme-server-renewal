98e128c5c3a46a453c28ce34b756a79badf9921a
thguss <thgus345@naver.com>
2023-01-13 23:28:09 +0900

    [FEAT] 서버 연결 테스트 API 구현

---
+++ src/main/java/com/smeme/server/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.smeme.server.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String test() {
+        return "서버 연결";
+    }
+}
