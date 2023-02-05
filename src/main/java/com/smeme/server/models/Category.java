66e3d667660f9c72cdf8a764c3ec6389a9f07bdb
thguss <thgus345@naver.com>
2023-02-05 17:46:44 +0900

    [ADD] id 값 자동 생성 전략 옵션 추가

--- src/main/java/com/smeme/server/models/Category.java
+++ src/main/java/com/smeme/server/models/Category.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Getter
 public class Category {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Long id;
 
