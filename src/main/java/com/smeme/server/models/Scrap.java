21e717d893306b1eaeb4a4e7966f8741c8a92716
thguss <thgus345@naver.com>
2023-02-05 17:52:08 +0900

    [FIX] 연관관계 편의 메소드 생성자 로직으로 처리

--- src/main/java/com/smeme/server/models/Scrap.java
+++ src/main/java/com/smeme/server/models/Scrap.java
@@ -2,6 +2,7 @@ package com.smeme.server.models;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -32,21 +33,23 @@ public class Scrap {
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
-    public void setUser(User user) {
+    @Builder
+    public Scrap(User user, Diary diary, String paragraph) {
+
         if (Objects.nonNull(this.user)) {
             this.user.getScraps().remove(this);
         }
 
-        this.user = user;
-        user.getScraps().add(this);
-    }
-
-    public void setDiary(Diary diary) {
         if (Objects.nonNull(this.diary)) {
             this.diary.getScraps().remove(this);
         }
 
+        this.user = user;
         this.diary = diary;
+        this.paragraph = paragraph;
+        this.createdAt = LocalDateTime.now();
+
+        user.getScraps().add(this);
         diary.getScraps().add(this);
     }
 }
