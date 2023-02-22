d13a4a35eabe6bb07fcc44bf294e019a67be02ec
unanchoi <unusualc@likelion.org>
2023-02-23 03:07:09 +0900

    [CHORE] @NonNull Annotation 추가

--- src/main/java/com/smeme/server/dtos/auth/AuthGetTokenResponseDto.java
+++ src/main/java/com/smeme/server/dtos/auth/AuthGetTokenResponseDto.java
@@ -1,10 +1,13 @@
 package com.smeme.server.dtos.auth;
 
 import lombok.Builder;
+import lombok.NonNull;
 
 @Builder
 public record AuthGetTokenResponseDto(
+        @NonNull
         String accessToken,
+        @NonNull
         String refreshToken
 ) {
 }
