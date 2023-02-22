868d1a1a1a2a259c10587fbb04a54dd3bdb056eb
unanchoi <unusualc@likelion.org>
2023-02-22 22:58:51 +0900

    [CHORE] Social 로그인 관련 설정

---
+++ src/main/java/com/smeme/server/config/jwt/JwtTokenFilter.java
@@ -0,0 +1,57 @@
+package com.smeme.server.config.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final String exceptionProperty = "exception";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = getJwtFromRequest(request);
+            JwtValidationType jwtValidationType = jwtTokenProvider.validateToken(jwt);
+            if ( StringUtils.hasText(jwt) && jwtValidationType == JwtValidationType.VALID_JWT) {
+                Long userId = jwtTokenProvider.getUserFromJwt(jwt);
+
+                UserAuthentication authentication = new UserAuthentication(userId, null, null);
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            } else {
+                request.setAttribute("UnAuthorization", jwtTokenProvider);
+                request.setAttribute("UnAuthorization", jwtTokenProvider.validateToken(jwt));
+            }
+        } catch (Exception exception) {
+            log.error("error : ", exception);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring("Bearer ".length());
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/com/smeme/server/config/jwt/JwtValidationType.java
@@ -0,0 +1,10 @@
+package com.smeme.server.config.jwt;
+
+public enum JwtValidationType {
+    VALID_JWT,
+    INVALID_JWT_SIGNATURE,
+    INVALID_JWT_TOKEN,
+    EXPIRED_JWT_TOKEN,
+    UNSUPPORTED_JWT_TOKEN,
+    EMPTY_JWT
+}
