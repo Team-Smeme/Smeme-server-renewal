16fe344fb5b7e92dfb1bb2397d8beae7789152c8
thguss <thgus345@naver.com>
2023-04-26 16:58:39 +0900

    [DEL] repository 이전 파일 삭제

--- src/main/java/com/smeme/server/repositories/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.smeme.server.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
