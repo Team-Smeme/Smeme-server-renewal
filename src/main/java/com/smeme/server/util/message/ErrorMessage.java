05491af2aad97c3dd81f8c87eb373144139f9732
thguss <thgus345@naver.com>
2023-05-06 03:16:12 +0900

    [ADD] 예외처리 추가

--- src/main/java/com/smeme/server/util/message/ErrorMessage.java
+++ src/main/java/com/smeme/server/util/message/ErrorMessage.java
@@ -1,4 +1,23 @@
 package com.smeme.server.util.message;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ErrorMessage {
+	// auth
+	EMPTY_ACCESS_TOKEN("액세스 토큰이 없습니다."),
+
+	// member
+	INVALID_MEMBER("유효하지 않은 회원입니다."),
+
+	// diary
+	EXIST_TODAY_DIARY("일기는 하루에 한 번씩 작성할 수 있습니다."),
+
+	// topic
+	INVALID_TOPIC("유효하지 않은 랜덤주제입니다.")
+	;
+
+	private final String message;
 }
