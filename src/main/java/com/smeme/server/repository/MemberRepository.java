22f8e84aa696eaafdd86731e14f7efd42466116f
unsualc@likelion.org <unusualc@likelion.org>
2023-05-09 01:26:07 +0900

    [CHORE] member repository method 추가

--- src/main/java/com/smeme/server/repository/MemberRepository.java
+++ src/main/java/com/smeme/server/repository/MemberRepository.java
@@ -1,2 +1,13 @@
-package com.smeme.server.repository;public interface MemberRepository {
+package com.smeme.server.repository;
+
+import com.smeme.server.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsBySocialId(String socialId);
+
+    Optional<Member> findBySocialId(String socialId);
 }
