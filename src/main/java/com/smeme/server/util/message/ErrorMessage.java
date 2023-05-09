9d9ea87d3f28baa7ffde65dabad08277055ba3be
unsualc@likelion.org <unusualc@likelion.org>
2023-05-09 21:20:40 +0900

    [CHORE] response message 추가

--- src/main/java/com/smeme/server/util/message/ErrorMessage.java
+++ src/main/java/com/smeme/server/util/message/ErrorMessage.java
@@ -8,7 +8,11 @@ import lombok.Getter;
 public enum ErrorMessage {
 
     // jwt
-    INVALID_TOKEN("유효하지 않은 토큰");
+    INVALID_TOKEN("유효하지 않은 토큰"),
+    EMPTY_REFRESH_TOKEN("리프레시 토큰이 없습니다"),
+
+    // member
+    INVALID_MEMBER("유효하지 않은 회원입니다.");
 
     private String message;
 }
--- src/main/java/com/smeme/server/util/message/ResponseMessage.java
+++ src/main/java/com/smeme/server/util/message/ResponseMessage.java
@@ -10,7 +10,10 @@ public enum ResponseMessage {
 	SUCCESS_PUSH_MESSAGE("푸시알람 요청 성공"),
 
 	// oauth
-	SUCCESS_SIGNIN("소셜로그인 성공");
+	SUCCESS_SIGNIN("소셜로그인 성공"),
+
+	// jwt
+	SUCCESS_ISSUE_TOKEN("토큰 발급 성공");
 
 	private final String message;
 }
