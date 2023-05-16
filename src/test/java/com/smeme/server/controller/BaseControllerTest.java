21132a848b43387f9561b4ea14b21fd281ee0e6d
unsualc@likelion.org <unusualc@likelion.org>
2023-05-17 01:33:09 +0900

    [FEAT] TestController로 RestDocs test

--- src/test/java/com/smeme/server/controller/BaseControllerTest.java
+++ src/test/java/com/smeme/server/controller/BaseControllerTest.java
@@ -1,2 +1,40 @@
-package com.smeme.server.controller;public class BaseControllerTest {
+package com.smeme.server.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@ExtendWith({RestDocumentationExtension.class})
+public abstract class BaseControllerTest {
+
+    @Autowired
+    protected WebApplicationContext context;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(final RestDocumentationContextProvider restDocumentation) {
+            mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(restDocumentation))
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                        .alwaysDo(print())
+                        .build();
+    }
 }
