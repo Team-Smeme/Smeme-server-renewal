a8cf41764b312e4cdd0b93f80b084ce0579b6e12
unsualc@likelion.org <unusualc@likelion.org>
2023-05-16 13:50:15 +0900

    [CHORE] UsernameValidator 수정

--- src/main/java/com/smeme/server/dto/member/UsernameValidator.java
+++ src/main/java/com/smeme/server/dto/member/UsernameValidator.java
@@ -11,18 +11,17 @@ public class UsernameValidator implements ConstraintValidator<ValidUsername, Str
 
     @Override
     public boolean isValid(String username, ConstraintValidatorContext context) {
-        if (username == null || username.isEmpty()) {
+
+        // null, 공백으로만 이뤄지는 경우, 빈 값인 경우 ''
+        if (username.isBlank()) {
             return false;
         }
 
+        // 길이가 1보다 작거나 10보다 큰 경우
         if (username.length() < 1 || username.length() > 10) {
             return false;
         }
-
-        if (username.charAt(0) == ' ') {
-            return false;
-        }
-
-        return username.trim().isEmpty();
+        // 첫 글자가 공백인 경우
+        return !(username.charAt(0) == ' ');
     }
 }
