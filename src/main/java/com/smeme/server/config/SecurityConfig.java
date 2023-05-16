8a73b54c8232425e91c4e41f6c3bd9d20f31ed42
unsualc@likelion.org <unusualc@likelion.org>
2023-05-17 01:29:14 +0900

    [CHORE] Security 설정

--- src/main/java/com/smeme/server/config/SecurityConfig.java
+++ src/main/java/com/smeme/server/config/SecurityConfig.java
@@ -1,29 +1,22 @@
 package com.smeme.server.config;
 
-import com.smeme.server.config.jwt.CustomJwtAuthenticationEntryPoint;
-import com.smeme.server.config.jwt.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final CustomJwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    private final JwtTokenFilter jwtTokenFilter;
 
     @Bean
-    @Profile("dev")
+    @Profile("!prod")
     SecurityFilterChain devFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
@@ -33,7 +26,6 @@ public class SecurityConfig {
                 .cors()
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .anyRequest().permitAll()
                 .and()
                 .build();
