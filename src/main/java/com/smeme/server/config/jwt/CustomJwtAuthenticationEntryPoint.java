031d39b8f5e9eb4fd5abf1b9af797484d673dc9f
unsualc@likelion.org <unusualc@likelion.org>
2023-05-31 15:00:15 +0900

    [CHORE] Jwt Security 설정

--- src/main/java/com/smeme/server/config/jwt/CustomJwtAuthenticationEntryPoint.java
+++ src/main/java/com/smeme/server/config/jwt/CustomJwtAuthenticationEntryPoint.java
@@ -1,20 +1,35 @@
 package com.smeme.server.config.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.smeme.server.util.ApiResponse;
+import com.smeme.server.util.message.ErrorMessage;
+import com.smeme.server.util.message.ResponseMessage;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class CustomJwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    private final String MESSAGE_UNAUTHORIZED = "유효하지 않은 토큰";
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, MESSAGE_UNAUTHORIZED);
+      setResponse(response);
+    }
+
+    private void setResponse(HttpServletResponse response) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().println(objectMapper.writeValueAsString(ApiResponse.fail(ErrorMessage.INVALID_TOKEN.getMessage())));
     }
 }
