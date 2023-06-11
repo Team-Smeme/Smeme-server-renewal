d0940119eb247920f6e9e8705ad1bf73469bb4f5
unsualc@likelion.org <unusualc@likelion.org>
2023-06-11 15:26:19 +0900

    [CHORE] 사용하지 않는 method 삭제

--- src/main/java/com/smeme/server/repository/trainingTime/TrainingTimeRepository.java
+++ src/main/java/com/smeme/server/repository/trainingTime/TrainingTimeRepository.java
@@ -5,11 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.smeme.server.model.training.TrainingTime;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TrainingTimeRepository extends JpaRepository<TrainingTime, Long>, TrainingTimeCustomRepository {
-
-    Optional<TrainingTime> findByMemberId(Long memberId);
 
     List<TrainingTime> findAllByMemberId(Long memberId);
 
