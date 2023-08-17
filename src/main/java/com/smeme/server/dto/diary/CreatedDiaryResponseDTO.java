c60bda658a9d4aedfab0f9ee500425f8bc73e3c6
thguss <thgus345@naver.com>
2023-08-18 02:18:57 +0900

    [ADD] 테스트용 메서드 추가

--- src/main/java/com/smeme/server/dto/diary/CreatedDiaryResponseDTO.java
+++ src/main/java/com/smeme/server/dto/diary/CreatedDiaryResponseDTO.java
@@ -14,6 +14,10 @@ public record CreatedDiaryResponseDTO(
 	public static CreatedDiaryResponseDTO of(Long diaryId, List<Badge> badges) {
 		return new CreatedDiaryResponseDTO(diaryId, badges.stream().map(BadgeDTO::of).toList());
 	}
+
+	public static CreatedDiaryResponseDTO testOf() {
+		return new CreatedDiaryResponseDTO(1L, List.of(BadgeDTO.testOf()));
+	}
 }
 
 record BadgeDTO(
@@ -22,5 +26,9 @@ record BadgeDTO(
 ) {
 	public static BadgeDTO of(Badge badge) {
 		return new BadgeDTO(badge.getName(), badge.getImageUrl());
+	}
+
+	public static BadgeDTO testOf() {
+		return new BadgeDTO("웰컴 뱃지", "image-url");
 	}
 }
