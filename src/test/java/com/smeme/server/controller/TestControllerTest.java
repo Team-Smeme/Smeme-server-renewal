d7141fb09e2345db4aecd7749e1b8f288180dad0
thguss <thgus345@naver.com>
2023-08-18 01:37:14 +0900

    [TEST] 서버 연결 및 푸시 알림 테스트 기능 테스트

--- src/test/java/com/smeme/server/controller/TestControllerTest.java
+++ src/test/java/com/smeme/server/controller/TestControllerTest.java
@@ -2,64 +2,99 @@ package com.smeme.server.controller;
 
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.smeme.server.util.ApiResponse;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
-import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static com.smeme.server.util.ApiResponse.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 
+import java.security.Principal;
+
 @DisplayName("TestController 테스트")
-@WebMvcTest(value = TestController.class)
+@WebMvcTest(TestController.class)
 class TestControllerTest extends BaseControllerTest {
 
-    private static final String DEFAULT_URL = "/api/v2/test";
+	private static final String DEFAULT_URL = "/api/v2/test";
 
-    @MockBean
-    TestController testController;
+	@MockBean
+	TestController testController;
+	@MockBean
+	Principal principal;
 
-    @Test
-    @DisplayName("서버 연결 테스트")
-    void success_test() throws Exception {
+	@Test
+	@DisplayName("서버 연결 테스트")
+	void success_server_connect_test() throws Exception {
+		//given
+		ResponseEntity<ApiResponse> response = ResponseEntity.ok(success("server connect"));
 
-        //given
-        when(testController.test())
-                .thenReturn(ResponseEntity.ok(ApiResponse.success("server connect", null)));
+		// when
+		when(testController.test()).thenReturn(response);
 
-        //then
-        ResultActions resultActions = mockMvc.perform(
-                RestDocumentationRequestBuilders.get(DEFAULT_URL)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                ).andDo(
-                MockMvcRestDocumentation.document(
-                        "test-docs",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        resource(
-                                ResourceSnippetParameters.builder()
-                                        .description("서버 연결 테스트")
-                                        .requestFields()
-                                        .responseFields(
-                                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("응답 성공 여부"),
-                                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
-                                                fieldWithPath("data").type(JsonFieldType.NULL).description("응답 데이터")
-                                        ).build()
-                )
-            )
-        );
-        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
-    }
+		//then
+		ResultActions resultActions = mockMvc
+			.perform(get(DEFAULT_URL)
+				.contentType(APPLICATION_JSON)
+				.accept(APPLICATION_JSON));
+
+		resultActions
+			.andDo(document("server connect test",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				resource(
+					ResourceSnippetParameters.builder()
+						.description("서버 연결 테스트")
+						.requestFields()
+						.responseFields(
+							fieldWithPath("success").type(BOOLEAN).description("응답 성공 여부"),
+							fieldWithPath("message").type(STRING).description("응답 메시지"),
+							fieldWithPath("data").type(NULL).description("응답 데이터"))
+						.build())))
+			.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	@DisplayName("푸시 알림 테스트")
+	void success_push_alarm_test() throws Exception {
+		// given
+		ResponseEntity<ApiResponse> response = ResponseEntity.ok(success("푸시 알림 전송 성공"));
+
+		// when
+		when(testController.alarmTest(principal)).thenReturn(response);
+
+		// then
+		ResultActions resultActions = mockMvc
+			.perform(get(DEFAULT_URL + "/alarm")
+				.contentType(APPLICATION_JSON)
+				.accept(APPLICATION_JSON)
+				.principal(principal));
+
+		resultActions
+			.andDo(document("push alarm test",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				resource(
+					ResourceSnippetParameters.builder()
+						.description("푸시 알림 테스트")
+						.requestFields()
+						.responseFields(
+							fieldWithPath("success").type(BOOLEAN).description("응답 성공 여부"),
+							fieldWithPath("message").type(STRING).description("응답 메시지"),
+							fieldWithPath("data").type(NULL).description("응답 데이터"))
+						.build())))
+			.andExpect(MockMvcResultMatchers.status().isOk());
+	}
 }
 
