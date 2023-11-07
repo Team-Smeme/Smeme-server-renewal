f7f67402607d393e16d667166f772d9e2716dd97
thguss <thgus345@naver.com>
2023-11-07 22:37:01 +0900

    [TEST] 일기 작성 테스트

--- src/main/java/com/smeme/server/dto/diary/CreatedDiaryResponseDTO.java
+++ src/main/java/com/smeme/server/dto/diary/CreatedDiaryResponseDTO.java
@@ -4,13 +4,8 @@ import java.util.List;
 
 import com.smeme.server.model.badge.Badge;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(description = "일기 생성 응답 DTO")
 public record CreatedDiaryResponseDTO(
-        @Schema(description = "생성된 일기 id", example = "1")
         Long diaryId,
-        @Schema(description = "획득한 뱃지 리스트")
         List<BadgeDTO> badges
 ) {
     public static CreatedDiaryResponseDTO of(Long diaryId, List<Badge> badges) {
@@ -18,9 +13,7 @@ public record CreatedDiaryResponseDTO(
     }
 
     public record BadgeDTO(
-            @Schema(description = "뱃지 이름", example = "웰컴 뱃지")
             String name,
-            @Schema(description = "뱃지 이미지 url", example = "image-url")
             String imageUrl
     ) {
         public static BadgeDTO of(Badge badge) {
