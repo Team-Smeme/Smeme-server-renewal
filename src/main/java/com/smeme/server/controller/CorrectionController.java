37e15e8ef836d56da29321af82a15720c4b6e9d8
thguss <thgus345@naver.com>
2023-11-07 23:35:45 +0900

    [DEL] 첨삭(미사용 API) 스웨거 어노테이션 삭제

--- src/main/java/com/smeme/server/controller/CorrectionController.java
+++ src/main/java/com/smeme/server/controller/CorrectionController.java
@@ -7,7 +7,6 @@ import static com.smeme.server.util.message.ResponseMessage.*;
 
 import java.security.Principal;
 
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,30 +22,20 @@ import com.smeme.server.service.CorrectionService;
 import com.smeme.server.util.ApiResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
-@Tag(name = "Correction", description = "첨삭 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v2/corrections")
-@SecurityRequirement(name = "Authorization")
 public class CorrectionController {
 
     private final CorrectionService correctionService;
 
-    @Operation(summary = "일기 첨삭", description = "일기 첨삭을 생성합니다.")
-    @ApiResponses({
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(
-                    responseCode = "201",
-                    description = "일기 첨삭 성공")
-    })
+    @Operation(description = "일기 첨삭")
     @PostMapping("/diary/{diaryId}")
     public ResponseEntity<ApiResponse> save(
-            @Parameter(hidden = true) Principal principal,
-            @Parameter(description = "일기 id") @PathVariable Long diaryId,
+            Principal principal,
+            @PathVariable Long diaryId,
             @RequestBody CorrectionRequestDTO request
     ) {
         CorrectionResponseDTO response = correctionService.save(getMemberId(principal), diaryId, request);
@@ -55,26 +44,16 @@ public class CorrectionController {
                 .body(success(SUCCESS_CREATE_CORRECTION.getMessage(), response));
     }
 
-    @Operation(summary = "첨삭 삭제", description = "첨삭을 삭제합니다.")
-    @ApiResponses({
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(
-                    responseCode = "200",
-                    description = "첨삭 삭제 성공")
-    })
+    @Operation(description = "첨삭 삭제")
     @DeleteMapping("/{correctionId}")
-    public ResponseEntity<ApiResponse> delete(@Parameter(description = "첨삭 id") @PathVariable Long correctionId) {
+    public ResponseEntity<ApiResponse> delete(@PathVariable Long correctionId) {
         correctionService.delete(correctionId);
         return ResponseEntity.ok(success(SUCCESS_DELETE_CORRECTION.getMessage()));
     }
 
-    @Operation(summary = "첨삭 수정", description = "첨삭을 수정합니다.")
-    @ApiResponses({
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(
-                    responseCode = "200",
-                    description = "첨삭 수정 성공")
-    })
+    @Operation(description = "첨삭 수정")
     @PatchMapping("/{correctionId}")
-    public ResponseEntity<ApiResponse> update(@Parameter(description = "첨삭 id") @PathVariable Long correctionId, @RequestBody CorrectionRequestDTO request) {
+    public ResponseEntity<ApiResponse> update(@PathVariable Long correctionId, @RequestBody CorrectionRequestDTO request) {
         correctionService.update(correctionId, request);
         return ResponseEntity.ok(success(SUCCESS_UPDATE_CORRECTION.getMessage()));
     }
