dd8cbca0cdfe9b5e805d185cd330cd48171f9dd0
thguss <thgus345@naver.com>
2023-11-07 23:56:57 +0900

    [TEST] 학습 목표 테스트

--- src/main/java/com/smeme/server/dto/goal/GoalsResponseDTO.java
+++ src/main/java/com/smeme/server/dto/goal/GoalsResponseDTO.java
@@ -4,20 +4,15 @@ import java.util.List;
 
 import com.smeme.server.model.goal.GoalType;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
 public record GoalsResponseDTO(
-        @Schema(description = "학습 목표 리스트")
         List<GoalResponseVO> goals
 ) {
     public static GoalsResponseDTO of(List<GoalType> goalTypes) {
         return new GoalsResponseDTO(goalTypes.stream().map(GoalResponseVO::of).toList());
     }
 
-    record GoalResponseVO(
-            @Schema(description = "학습 목표 ENUM 값", example = "APPLY")
+    public record GoalResponseVO(
             String goalType,
-            @Schema(description = "학습 목표 이름", example = "현지 언어 체득")
             String name
     ) {
         public static GoalResponseVO of(GoalType goalType) {
