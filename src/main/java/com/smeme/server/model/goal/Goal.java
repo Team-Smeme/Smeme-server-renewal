ef020748fab8a07e67baf18e7518772f22dea713
thguss <thgus345@naver.com>
2023-12-05 22:53:04 +0900

    [ADD] 기본 생성자 추가

--- src/main/java/com/smeme/server/model/goal/Goal.java
+++ src/main/java/com/smeme/server/model/goal/Goal.java
@@ -8,11 +8,14 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Goal {
 
     @Id
