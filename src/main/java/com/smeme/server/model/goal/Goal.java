23dda0dd45c176bddc0f05c8f0f152e3638207d8
thguss <thgus345@naver.com>
2023-12-05 22:54:57 +0900

    [DEL] 미사용 임포터 삭제

--- src/main/java/com/smeme/server/model/goal/Goal.java
+++ src/main/java/com/smeme/server/model/goal/Goal.java
@@ -8,7 +8,6 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
