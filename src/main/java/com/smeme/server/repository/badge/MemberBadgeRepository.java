0a17961ee6294a6fa3e7ae32c549b2c815f98a53
thguss <thgus345@naver.com>
2024-01-17 03:14:11 +0900

    [DELETE] 미사용 메서드 삭제

--- src/main/java/com/smeme/server/repository/badge/MemberBadgeRepository.java
+++ src/main/java/com/smeme/server/repository/badge/MemberBadgeRepository.java
@@ -1,7 +1,4 @@
 package com.smeme.server.repository.badge;
-
-import com.smeme.server.model.Member;
-import com.smeme.server.model.badge.Badge;
 import com.smeme.server.model.badge.MemberBadge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,10 +8,5 @@ import java.util.Optional;
 public interface MemberBadgeRepository extends JpaRepository<MemberBadge, Long> {
 
     List<MemberBadge> findAllByMemberId(Long memberId);
-
-    boolean existsByMemberAndBadge(Member member, Badge badge);
-
     Optional<MemberBadge> findFirstByMemberIdOrderByCreatedAtDesc(Long memberId);
-
-    void deleteAllByMemberId(Long id);
 }
