35f935a58572022c06b81d7054bb592c063b91f7
thguss <thgus345@naver.com>
2024-03-12 23:51:58 +0900

    [ADD] 스웨거 컨벤션 통일 반영

--- smeem-api/src/main/java/com/smeem/api/test/api/TestApi.java
+++ smeem-api/src/main/java/com/smeem/api/test/api/TestApi.java
@@ -2,29 +2,34 @@ package com.smeem.api.test.api;
 
 import com.smeem.api.common.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
 import java.security.Principal;
 
+import static io.swagger.v3.oas.annotations.enums.ParameterIn.HEADER;
+
+@Tag(name = "[Test] 테스트 관련 API (V2)")
 public interface TestApi {
 
-    @Operation(
-            summary = "서버 연결 테스트",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "성공"),
-                    @ApiResponse(responseCode = "500", description = "서버 내부 오류")
-            }
-    )
+    @Operation(summary = "서버 연결 테스트 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "4xx", description = "유효하지 않은 요청"),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
     ResponseEntity<BaseResponse<?>> test();
 
-    @Operation(
-            summary = "푸시알림 테스트",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "성공"),
-                    @ApiResponse(responseCode = "401", description = "유효하지 않은 토큰"),
-                    @ApiResponse(responseCode = "500", description = "서버 내부 오류")
-            }
-    )
+    @Operation(summary = "푸시알림 테스트 API")
+    @Parameter(name = "Authorization", description = "Bearer {access_token}", in = HEADER, required = true)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "4xx", description = "유효하지 않은 요청"),
+            @ApiResponse(responseCode = "401", description = "유효하지 않은 토큰"),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
     ResponseEntity<BaseResponse<?>> alarmTest(Principal principal);
 }
--- smeem-api/src/main/java/com/smeem/api/topic/api/TopicApi.java
+++ smeem-api/src/main/java/com/smeem/api/topic/api/TopicApi.java
@@ -2,17 +2,24 @@ package com.smeem.api.topic.api;
 
 import com.smeem.api.common.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
+import static io.swagger.v3.oas.annotations.enums.ParameterIn.HEADER;
+
+@Tag(name = "[Topic] 주제 관련 API (V2)")
 public interface TopicApi {
 
-    @Operation(
-            summary = "랜덤 주제 조회",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "성공"),
-                    @ApiResponse(responseCode = "500", description = "서버 내부 오류")
-            }
-    )
+    @Operation(summary = "랜덤 주제 조회 API")
+    @Parameter(name = "Authorization", description = "Bearer {access_token}", in = HEADER, required = true)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "4xx", description = "유효하지 않은 요청"),
+            @ApiResponse(responseCode = "401", description = "유효하지 않은 토큰"),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
     ResponseEntity<BaseResponse<?>> getTopicByRandom();
 }
