918bc515829798b11bdd64a616d12b15fa7fbde9
thguss <thgus345@naver.com>
2024-03-22 02:52:28 +0900

    [REFACTOR] 리팩토링 반영

--- smeem-api/src/main/java/com/smeem/api/test/service/TestService.java
+++ smeem-api/src/main/java/com/smeem/api/test/service/TestService.java
@@ -5,8 +5,8 @@ import com.smeem.common.config.ValueConfig;
 import com.smeem.common.exception.MemberException;
 import com.smeem.domain.member.model.Member;
 import com.smeem.domain.member.repository.MemberRepository;
-import com.smeem.external.firebase.FcmService;
-import com.smeem.external.firebase.dto.request.MessagePushServiceRequest;
+import com.smeem.external.firebase.NotificationService;
+import com.smeem.external.firebase.dto.request.NotificationSingleRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.stereotype.Service;
@@ -21,14 +21,14 @@ public class TestService {
 
     private final MemberRepository memberRepository;
 
-    private final FcmService fcmService;
+    private final NotificationService notificationService;
     private final ValueConfig valueConfig;
 
     public void pushTest(final TestPushAlarmServiceRequest request) {
         val member = findMember(request.memberId());
         val title = valueConfig.getMESSAGE_TITLE();
         val body = valueConfig.getMESSAGE_BODY();
-        fcmService.pushMessage(MessagePushServiceRequest.of(member.getFcmToken(), title, body));
+        notificationService.sendMessage(NotificationSingleRequest.of(member.getFcmToken(), title, body));
     }
 
     private Member findMember(long id) {
