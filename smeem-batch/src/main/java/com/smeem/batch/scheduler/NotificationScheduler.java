9a315bdd18a753260983e9efa226243fac8a1ec8
thguss <thgus345@naver.com>
2024-03-22 02:53:26 +0900

    [REFACTOR] 리팩토링 반영, 레포지토리 쿼리 수정

---
+++ smeem-batch/src/main/java/com/smeem/batch/scheduler/NotificationScheduler.java
@@ -0,0 +1,41 @@
+package com.smeem.batch.scheduler;
+
+import com.smeem.common.config.ValueConfig;
+import com.smeem.domain.member.model.Member;
+import com.smeem.domain.member.repository.MemberRepository;
+import com.smeem.external.firebase.NotificationService;
+import com.smeem.external.firebase.dto.request.NotificationMulticastRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Component
+@EnableScheduling
+@RequiredArgsConstructor
+public class NotificationScheduler {
+
+    private final MemberRepository memberRepository;
+
+    private final NotificationService notificationService;
+    private final ValueConfig valueConfig;
+
+    @Scheduled(cron = "${fcm.cron_expression}")
+    @Transactional(readOnly = true)
+    public void sendMessagesForTrainingTime() throws InterruptedException {
+        Thread.sleep(1000);
+
+        val members = memberRepository.findAllByTrainingTimeForSendingMessage(LocalDateTime.now());
+
+        if (!members.isEmpty()) {
+            val tokens = members.stream().map(Member::getFcmToken).toList();
+            val messageTitle = valueConfig.getMESSAGE_TITLE();
+            val messageBody = valueConfig.getMESSAGE_BODY();
+            notificationService.sendMessages(NotificationMulticastRequest.of(tokens, messageTitle, messageBody));
+        }
+    }
+}
