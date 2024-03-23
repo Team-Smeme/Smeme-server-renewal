e43ec622cd4da6422f87ea6c99d98f571911a7d3
unanchoi <unanc2@gmail.com>
2024-03-23 22:05:01 +0900

    [CHORE] Sentry 설정 추가

--- smeem-api/src/main/java/com/smeem/api/common/advicer/ErrorHandler.java
+++ smeem-api/src/main/java/com/smeem/api/common/advicer/ErrorHandler.java
@@ -4,6 +4,7 @@ import com.smeem.api.common.ApiResponseUtil;
 import com.smeem.api.common.dto.FailureResponse;
 import com.smeem.common.exception.*;
 import com.smeem.external.discord.DiscordAlarmSender;
+import io.sentry.Sentry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,61 +21,72 @@ public class ErrorHandler {
 
     @ExceptionHandler(TrainingTimeException.class)
     public ResponseEntity<FailureResponse> trainingTimeException(TrainingTimeException exception) {
+        Sentry.captureException(exception);
         return ApiResponseUtil.failure(exception.getFailureCode());
     }
 
     @ExceptionHandler(TokenException.class)
     public ResponseEntity<FailureResponse> tokenException(TokenException exception) {
+        Sentry.captureException(exception);
         return ApiResponseUtil.failure(exception.getFailureCode());
     }
 
     @ExceptionHandler(BadgeException.class)
     public ResponseEntity<FailureResponse> badgeException(BadgeException exception) {
+        Sentry.captureException(exception);
         return ApiResponseUtil.failure(exception.getFailureCode());
     }
 
     @ExceptionHandler(DiaryException.class)
     public ResponseEntity<FailureResponse> diaryException(DiaryException exception) {
+        Sentry.captureException(exception);
         return ApiResponseUtil.failure(exception.getFailureCode());
     }
 
     @ExceptionHandler(MemberException.class)
     public ResponseEntity<FailureResponse> memberException(MemberException exception) {
+        Sentry.captureException(exception);
         return ApiResponseUtil.failure(exception.getFailureCode());
     }
 
     @ExceptionHandler(GoalException.class)
     public ResponseEntity<FailureResponse> goalException(GoalException exception) {
+        Sentry.captureException(exception);
         return ApiResponseUtil.failure(exception.getFailureCode());
     }
 
     @ExceptionHandler(TopicException.class)
     public ResponseEntity<FailureResponse> topicException(TopicException exception) {
+        Sentry.captureException(exception);
         return ApiResponseUtil.failure(exception.getFailureCode());
     }
 
     @ExceptionHandler(AppleException.class)
     public ResponseEntity<FailureResponse> appleException(AppleException exception) {
+        Sentry.captureException(exception);
         return ApiResponseUtil.failure(exception.getFailureCode());
     }
 
     @ExceptionHandler(FcmException.class)
     public ResponseEntity<FailureResponse> fcmException(FcmException exception) {
+        Sentry.captureException(exception);
         return ApiResponseUtil.failure(exception.getFailureCode());
     }
 
     @ExceptionHandler(AuthException.class)
     public ResponseEntity<FailureResponse> authException(AuthException exception) {
+        Sentry.captureException(exception);
         return ApiResponseUtil.failure(exception.getFailureCode());
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<FailureResponse> baseException(RuntimeException exception) {
         sendDiscordAlarm(exception);
+        Sentry.captureException(exception);
         return ApiResponseUtil.failure(SERVER_ERROR);
     }
 
     private void sendDiscordAlarm(RuntimeException exception) {
-            discordAlarmSender.send(exception.getMessage(), ERROR);
+        discordAlarmSender.send(exception.getMessage(), ERROR);
     }
 }
