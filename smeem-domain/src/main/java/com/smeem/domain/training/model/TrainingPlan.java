3692adc74076623cd6713118bc6f609dd32eb07c
thguss <thgus345@naver.com>
2024-04-06 00:55:26 +0900

    [ADD] 트레이닝 플랜 테이블 추가

---
+++ smeem-domain/src/main/java/com/smeem/domain/training/model/TrainingPlan.java
@@ -0,0 +1,22 @@
+package com.smeem.domain.training.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@Getter
+public class TrainingPlan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int weeklyDiaryCount;
+}
