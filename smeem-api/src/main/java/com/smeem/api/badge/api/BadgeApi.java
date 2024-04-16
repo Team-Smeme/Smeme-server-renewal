7504b3bfdddc762041f9a148b920b5ac798cf854
unanchoi <unanc2@gmail.com>
2024-04-16 22:16:24 +0900

    [FEAT] 뱃지 조회 API controller 구현

--- smeem-api/src/main/java/com/smeem/api/badge/api/BadgeApi.java
+++ smeem-api/src/main/java/com/smeem/api/badge/api/BadgeApi.java
@@ -1,5 +1,6 @@
 package com.smeem.api.badge.api;
 
+import com.smeem.api.badge.api.dto.response.BadgeGetResponse;
 import com.smeem.api.badge.api.dto.response.BadgeListResponse;
 import com.smeem.api.common.FailureResponse;
 import com.smeem.api.common.SuccessResponse;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.security.Principal;
 
@@ -19,6 +21,15 @@ import static io.swagger.v3.oas.annotations.enums.ParameterIn.HEADER;
 
 @Tag(name = "[Badge] 뱃지 관련 API (V2)")
 public interface BadgeApi {
+
+    @Operation(summary = "뱃지 상세 조회 API")
+    @Parameter(name = "Authorization", description = "Bearer ${Smeme Access Token}", in = HEADER, required = true)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "뱃지 조회 성공"),
+            @ApiResponse(responseCode = "4011", description = "유효하지 않은 토큰입니다", content = @Content(schema = @Schema(implementation = FailureResponse.class))),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류", content = @Content(schema = @Schema(implementation = FailureResponse.class)))
+    })
+    ResponseEntity<SuccessResponse<BadgeGetResponse>> getBadgeById(Principal principal, @PathVariable Long badgeId);
 
     @Operation(summary = "뱃지 목록 조회 API")
     @Parameter(name = "Authorization", description = "Bearer ${Smeme Access Token}", in = HEADER, required = true)
--- smeem-api/src/main/java/com/smeem/api/badge/api/BadgeController.java
+++ smeem-api/src/main/java/com/smeem/api/badge/api/BadgeController.java
@@ -1,5 +1,6 @@
 package com.smeem.api.badge.api;
 
+import com.smeem.api.badge.api.dto.response.BadgeGetResponse;
 import com.smeem.api.badge.api.dto.response.BadgeListResponse;
 import com.smeem.api.badge.service.BadgeService;
 import com.smeem.api.support.ApiResponseGenerator;
@@ -9,11 +10,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
+import static com.smeem.common.code.success.BadgeSuccessCode.SUCCESS_GET_BADGE;
 import static com.smeem.common.code.success.BadgeSuccessCode.SUCCESS_GET_BADGES;
 
 @RestController
@@ -22,6 +25,17 @@ import static com.smeem.common.code.success.BadgeSuccessCode.SUCCESS_GET_BADGES;
 public class BadgeController implements BadgeApi {
 
     private final BadgeService badgeService;
+
+    @Override
+    @GetMapping("/{badgeId}")
+    public ResponseEntity<SuccessResponse<BadgeGetResponse>> getBadgeById(
+            Principal principal,
+            @PathVariable Long badgeId
+    ) {
+        val memberId = PrincipalConverter.getMemberId(principal);
+        val response = BadgeGetResponse.from(badgeService.getBadge(badgeId, memberId));
+        return ApiResponseGenerator.success(SUCCESS_GET_BADGE, response);
+    }
 
     @Override
     @GetMapping
