e146ccadef0e038a343b56799af5bbcd13cfb67b
thguss <thgus345@naver.com>
2024-04-30 21:01:40 +0900

    [FIX] id ASC 정렬 추가

--- smeem-domain/src/main/java/com/smeem/domain/plan/repository/PlanRepository.java
+++ smeem-domain/src/main/java/com/smeem/domain/plan/repository/PlanRepository.java
@@ -2,11 +2,6 @@ package com.smeem.domain.plan.repository;
 
 import com.smeem.domain.plan.model.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
-    @Query("SELECT p FROM Plan p ORDER BY p.id")
-    List<Plan> findAllOrderByIdAsc();
 }
