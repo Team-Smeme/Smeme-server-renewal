8a231c96495a8e640414307df4ce6daa786b9d5f
thguss <thgus345@naver.com>
2024-06-30 16:47:40 +0900

    add: bootstrap-module

---
+++ smeem-bootstrap/src/main/java/com/smeem/bootstrap/SmeemBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.smeem.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmeemBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmeemBootstrapApplication.class, args);
+	}
+
+}
