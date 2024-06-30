891ab44dcf943105402cfe17d0411dc3b6821d3f
thguss <thgus345@naver.com>
2024-07-01 01:57:55 +0900

    add: persistence-module

---
+++ smeem-application/src/test/java/com/smeem/application/SmeemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smeem.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmeemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
