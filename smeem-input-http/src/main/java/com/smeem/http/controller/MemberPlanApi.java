f68949dd0f61882d9be94ad801b0192e20ab7b10
thguss <thgus345@naver.com>
2024-07-03 23:05:21 +0900

    add: member plan

---
+++ smeem-input-http/src/main/java/com/smeem/http/controller/MemberPlanApi.java
@@ -0,0 +1,9 @@
+package com.smeem.http.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MemberPlanApi {
+}
