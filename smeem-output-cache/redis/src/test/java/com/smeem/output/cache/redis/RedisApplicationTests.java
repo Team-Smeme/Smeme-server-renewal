42d43ab894f1fca1125cead000eaac856b883800
thguss <thgus345@naver.com>
2024-10-08 00:33:47 +0900

    feat: added cache module

---
+++ smeem-output-cache/redis/src/test/java/com/smeem/output/cache/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smeem.output.cache.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
