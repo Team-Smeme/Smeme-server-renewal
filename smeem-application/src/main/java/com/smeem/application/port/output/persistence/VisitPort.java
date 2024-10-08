043d87eaf13fadc61ae6ed8a5e0a6d1897de2876
thguss <thgus345@naver.com>
2024-10-08 16:41:14 +0900

    delete: deleted VisitEntity

--- smeem-application/src/main/java/com/smeem/application/port/output/persistence/VisitPort.java
+++
@@ -1,8 +0,0 @@
-package com.smeem.application.port.output.persistence;
-
-import com.smeem.application.domain.visit.Visit;
-
-public interface VisitPort {
-    void visit(Visit visit);
-    boolean isExistByMemberAndToday(long memberId);
-}
