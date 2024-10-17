6cc01c04cc7c20ea9af6c815366a312b1de5c687
thguss <thgus345@naver.com>
2024-10-17 17:47:29 +0900

    fix: updated method name

--- smeem-output-cache/redis/src/main/java/com/smeem/output/cache/redis/adapter/RedisCacheAdapter.java
+++ smeem-output-cache/redis/src/main/java/com/smeem/output/cache/redis/adapter/RedisCacheAdapter.java
@@ -5,10 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.Optional;
+
+//TODO: Redis 죽으면 어떻게 할 것인가?
 @Configuration
 @RequiredArgsConstructor
-public class CacheAdapter implements CachePort {
+public class RedisCacheAdapter implements CachePort {
     private final RedisTemplate<String, String> redisTemplate;
+    private final RedisTemplate<String, Integer> integerRedisTemplate;
 
     @Override
     public void setBit(String key, long offset, boolean value) {
@@ -18,5 +22,20 @@ public class CacheAdapter implements CachePort {
     @Override
     public boolean getBit(String key, long offset) {
         return Boolean.TRUE.equals(redisTemplate.opsForValue().getBit(key, offset));
+    }
+
+    @Override
+    public Optional<Integer> getInt(String key) {
+        return Optional.ofNullable(integerRedisTemplate.opsForValue().get(key));
+    }
+
+    @Override
+    public void setInt(String key, int value) {
+        integerRedisTemplate.opsForValue().set(key, value);
+    }
+
+    @Override
+    public void incrementInt(String key) {
+        redisTemplate.opsForValue().increment(key);
     }
 }
