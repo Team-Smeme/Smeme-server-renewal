b6eb0d7d200241851646ac35b7182a04eb748952
thguss <thgus345@naver.com>
2024-10-17 17:58:03 +0900

    fix: updated field, method

--- smeem-application/src/main/java/com/smeem/application/domain/diary/Diary.java
+++ smeem-application/src/main/java/com/smeem/application/domain/diary/Diary.java
@@ -3,6 +3,8 @@ package com.smeem.application.domain.diary;
 import com.smeem.application.domain.generic.LangType;
 import com.smeem.application.domain.member.Member;
 import com.smeem.application.domain.topic.Topic;
+import com.smeem.common.exception.ExceptionCode;
+import com.smeem.common.exception.SmeemException;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,10 +13,17 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 public class Diary {
-    Long id;
-    String content;
-    LangType targetLang;
-    Topic topic;
-    Member member;
-    LocalDateTime createdAt;
+    private Long id;
+    private String content;
+    private LangType targetLang;
+    private Topic topic;
+    private Member member;
+    private long memberId;
+    private LocalDateTime createdAt;
+
+    public void validateDiaryOwnership(long memberId) {
+        if (this.memberId != memberId) {
+            throw new SmeemException(ExceptionCode.INVALID_MEMBER_AND_DIARY);
+        }
+    }
 }
