80ba168177e4e76314eb7358a44267a3bacb6526
thguss <thgus345@naver.com>
2024-10-17 17:48:10 +0900

    add: added for <String, Int>

--- smeem-application/src/main/java/com/smeem/application/port/output/cache/CachePort.java
+++ smeem-application/src/main/java/com/smeem/application/port/output/cache/CachePort.java
@@ -1,6 +1,12 @@
 package com.smeem.application.port.output.cache;
 
+import java.util.Optional;
+
 public interface CachePort {
     void setBit(String key, long offset, boolean value);
     boolean getBit(String key, long offset);
+
+    Optional<Integer> getInt(String key);
+    void setInt(String key, int value);
+    void incrementInt(String key);
 }
