05a4f92e6981aa26580c4e2da358fc353394a741
thguss <thgus345@naver.com>
2024-10-17 16:59:00 +0900

    add: added query

--- smeem-output-persistence/postgresql/src/main/java/com/smeem/persistence/postgresql/persistence/repository/diary/custom/CorrectionCustomRepository.java
+++ smeem-output-persistence/postgresql/src/main/java/com/smeem/persistence/postgresql/persistence/repository/diary/custom/CorrectionCustomRepository.java
@@ -1,4 +1,7 @@
 package com.smeem.persistence.postgresql.persistence.repository.diary.custom;
 
+import java.time.LocalDate;
+
 public interface CorrectionCustomRepository {
+    int countDistinctKeyByMemberIdAndCreatedAt(long memberId, LocalDate date);
 }
--- smeem-output-persistence/postgresql/src/main/java/com/smeem/persistence/postgresql/persistence/repository/diary/custom/CorrectionRepositoryImpl.java
+++ smeem-output-persistence/postgresql/src/main/java/com/smeem/persistence/postgresql/persistence/repository/diary/custom/CorrectionRepositoryImpl.java
@@ -4,8 +4,30 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static com.smeem.persistence.postgresql.persistence.entity.QCorrectionEntity.correctionEntity;
+
 @Repository
 @RequiredArgsConstructor
 public class CorrectionRepositoryImpl implements CorrectionCustomRepository {
     private final JPAQueryFactory queryFactory;
+
+    @Override
+    public int countDistinctKeyByMemberIdAndCreatedAt(long memberId, LocalDate date) {
+        LocalDateTime startOfDay = date.atStartOfDay();
+        LocalDateTime endOfDay = date.atTime(LocalTime.MAX);
+
+        return (int) queryFactory
+                .select(correctionEntity.key)
+                .from(correctionEntity)
+                .where(
+                        correctionEntity.createdAt.between(startOfDay, endOfDay),
+                        correctionEntity.memberId.eq(memberId)
+                )
+                .distinct()
+                .stream().count();
+    }
 }
