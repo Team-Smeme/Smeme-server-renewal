bad9860231f16ea17102801865467514c4382f30
thguss <thgus345@naver.com>
2024-10-17 17:02:27 +0900

    add: added method for correction

---
+++ smeem-application/src/main/java/com/smeem/application/port/output/persistence/CorrectionPort.java
@@ -0,0 +1,13 @@
+package com.smeem.application.port.output.persistence;
+
+import com.smeem.application.domain.diary.Correction;
+import com.smeem.application.domain.diary.Diary;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface CorrectionPort {
+    List<Correction> saveAll(List<Correction> corrections, Diary diary);
+
+    int countDistinctByMemberAndDate(long memberId, LocalDate date);
+}
---
+++ smeem-output-persistence/postgresql/src/main/java/com/smeem/persistence/postgresql/adapter/CorrectionAdapter.java
@@ -0,0 +1,32 @@
+package com.smeem.persistence.postgresql.adapter;
+
+import com.smeem.application.domain.diary.Correction;
+import com.smeem.application.domain.diary.Diary;
+import com.smeem.application.port.output.persistence.CorrectionPort;
+import com.smeem.persistence.postgresql.persistence.entity.CorrectionEntity;
+import com.smeem.persistence.postgresql.persistence.repository.diary.CorrectionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class CorrectionAdapter implements CorrectionPort {
+    private final CorrectionRepository correctionRepository;
+
+    @Override
+    public List<Correction> saveAll(List<Correction> corrections, Diary diary) {
+        UUID key = UUID.randomUUID();
+        return correctionRepository.saveAll(
+                corrections.stream().map(it -> new CorrectionEntity(it, diary, key)).toList()
+        ).stream().map(CorrectionEntity::toDomain).toList();
+    }
+
+    @Override
+    public int countDistinctByMemberAndDate(long memberId, LocalDate date) {
+        return correctionRepository.countDistinctKeyByMemberIdAndCreatedAt(memberId, date);
+    }
+}
