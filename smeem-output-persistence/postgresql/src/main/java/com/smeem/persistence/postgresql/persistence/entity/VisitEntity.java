e74c3e794fdbfa111d45ac706bc8bdf3b334b5ee
thguss <thgus345@naver.com>
2024-10-18 18:39:04 +0900

    add: added table option

--- smeem-output-persistence/postgresql/src/main/java/com/smeem/persistence/postgresql/persistence/entity/VisitEntity.java
+++ smeem-output-persistence/postgresql/src/main/java/com/smeem/persistence/postgresql/persistence/entity/VisitEntity.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "visit", schema = "smeem")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class VisitEntity {
     @Id
