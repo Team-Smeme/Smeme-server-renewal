cfca92a6c2b89a0082ee6bd88007f3934a66e09d
thguss <thgus345@naver.com>
2024-10-18 19:22:42 +0900

    delete: deleted completed task

--- smeem-input-http/src/main/java/com/smeem/http/config/SecurityConfig.java
+++ smeem-input-http/src/main/java/com/smeem/http/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
                         authorizeHttpRequests
                                 .requestMatchers(new AntPathRequestMatcher("/api/v2/auth", "POST")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/actuator/health")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/api/v2/test")).permitAll() //TODO: 삭제
+                                .requestMatchers(new AntPathRequestMatcher("/api/v2/test")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/api/v2/versions/client/app")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/api/v2/goals/{type}")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/api/v2/goals")).permitAll()
