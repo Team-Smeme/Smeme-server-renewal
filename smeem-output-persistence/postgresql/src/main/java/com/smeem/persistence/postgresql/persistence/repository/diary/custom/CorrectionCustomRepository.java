2919f3f9dab7da6770f1190d6df06544de18e234
thguss <thgus345@naver.com>
2024-10-26 16:03:23 +0900

    delete: deleted CorrectionEntity from postgresql

--- smeem-output-persistence/postgresql/src/main/java/com/smeem/persistence/postgresql/persistence/repository/diary/custom/CorrectionCustomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smeem.persistence.postgresql.persistence.repository.diary.custom;
-
-import java.time.LocalDate;
-
-public interface CorrectionCustomRepository {
-    int countDistinctKeyByMemberIdAndCreatedAt(long memberId, LocalDate date);
-}
