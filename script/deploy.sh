278c3913f7e1459854b3c6efc518068c1a6eb20a
thguss <thgus345@naver.com>
2025-07-16 00:48:14 +0900

    update

--- script/deploy.sh
+++ script/deploy.sh
@@ -19,8 +19,10 @@ echo "----------------------------------------------------------------------"
 
 # 도커 이미지 풀 받기
 echo "> Pull Docker Image"
+set +x
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+set -x
 docker pull "${REGISTRY_URL}"/"${IMAGE_NAME}":"${TAG}"
-echo "pull ${REGISTRY_URL}/${IMAGE_NAME}:${TAG}"
 
 # 서버 실행
 echo "> Run Docker"
